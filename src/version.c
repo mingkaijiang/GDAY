1ef6e376a63facbc26893fd3d23dab53b249e16a
mdekauwe <mdekauwe@gmail.com>
2015-03-17 22:36:38 +1100

    tidied up decs

--- src/version.c
+++ src/version.c
@@ -1,3 +1,3 @@
 #include "version.h"
-const char *build_git_sha = "2175cf83d8552bc3183b6b880317c6722c82b657";
-const char *build_git_time = "Tue 17 Mar 2015 21:16:21 AEDT";
+const char *build_git_sha = "85b8eacb99b255e4e3347fd48cbcb8527a3b4e3f";
+const char *build_git_time = "Tue 17 Mar 2015 22:35:35 AEDT";
